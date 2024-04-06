6a31484607c94f12a21fd8fae9adbd54c5ad1a5f
Mady Edwards <medwards2@ufl.edu>
2024-04-06 18:08:01 -0400

    slight change

--- winghacks24/src/components/hero/hero.js
+++ winghacks24/src/components/hero/hero.js
@@ -10,6 +10,7 @@ return (
         <h1>Welcome!</h1>
         <p>
             As Women in STEM, we feel it is important for us to recognize those who have paved the way before us. 
+            Their contributions towards their respective fields are invaluable, and their impact is nothing short of significant.
             Click to learn more about our history!
         </p>
         <Button buttonStyle={'btn--outline'} buttonSize={'btn--large'}>
