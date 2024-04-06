c3ff8398523b0037523bf84c4ad0d3f28454ba5c
Mady Edwards <medwards2@ufl.edu>
2024-04-06 16:04:09 -0400

    homepage some changes

---
+++ winghacks24/src/components/hero/hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./hero.css"
+
+function Hero() {
+
+return (
+    <div className="hero-container">
+        <h1>hhhh</h1>
+    </div>
+
+)
+   
+}
+export default Hero;
--- winghacks24/src/pages/homepage.js
+++ winghacks24/src/pages/homepage.js
@@ -1,10 +1,13 @@
 import '../components/temp.css'
 import { Button } from '../components/button/Button';
+import Hero from '../components/hero/hero';
 
 function Home() {
     return (
     <div className='page-body'>
-    
+    <Hero>
+        hi
+    </Hero>
     </div>
     );
 };
