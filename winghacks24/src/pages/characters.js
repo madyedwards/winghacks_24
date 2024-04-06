6ece5febec5c04e56121c0098271b84a5fa45094
shiishorts <elisaishome@gmail.com>
2024-04-06 16:49:29 -0400

    Minor button edit & character screen edits

--- winghacks24/src/pages/characters.js
+++ winghacks24/src/pages/characters.js
@@ -1,7 +1,19 @@
+import './characters.css';
+import { Button } from '../components/button/Button'
+
 function Characters() {
     return (
-    <div>
-        <p>FIGURES !</p>
+    <div className='character-container'>
+        <h1>CHOOSE YOUR FIGHTER!</h1>
+        <Button buttonStyle='btn--primary' buttonSize='btn--large'>Ada Lovelace</Button>
+        <div className='div-indent'></div>
+        <Button buttonStyle='btn--primary' buttonSize='btn--large'>Chien-Shiung Wu</Button>
+        <div className='div-indent'></div>
+        <Button buttonStyle='btn--primary' buttonSize='btn--large'>Dorothy Vaughan</Button>
+        <div className='div-indent'></div>
+        <Button buttonStyle='btn--primary' buttonSize='btn--large'>Lynn Conway</Button>
+        <div className='div-indent'></div>
+        <Button buttonStyle='btn--primary' buttonSize='btn--large'>SECRET LOCKED</Button>
     </div>
     );
 };
