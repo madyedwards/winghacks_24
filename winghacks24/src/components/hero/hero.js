8716e4435f3e687b562a114f28da4cb1dcb78fa6
Mady Edwards <medwards2@ufl.edu>
2024-04-06 16:28:21 -0400

    homepage changes

--- winghacks24/src/components/hero/hero.js
+++ winghacks24/src/components/hero/hero.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./hero.css"
+import { Button } from "../button/Button";
 
 function Hero() {
 
 return (
+    
     <div className="hero-container">
-        <h1>hhhh</h1>
+        <h1>Welcome!</h1>
+        <p>
+            As Women in STEM, we feel it is important for us to recognize those who have paved the way before us. 
+            Click to learn more about our history!
+        </p>
+        <Button buttonStyle={'btn--outline'} buttonSize={'btn--large'}>
+            Characters
+            </Button>
     </div>
 
 )
--- winghacks24/src/pages/homepage.js
+++ winghacks24/src/pages/homepage.js
@@ -4,11 +4,8 @@ import Hero from '../components/hero/hero';
 function Home() {
     return (
     <div>
-        <p>Welcome Home</p>
-
-        <Button buttonStyle='btn--outline' buttonSize='btn--large'>
-            Here is a button
-        </Button>
+        <Hero>
+        </Hero>
     </div>
     );
 };
